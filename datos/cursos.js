21ab1bce45c528789e06ad6320ea946693c8a115
200ximena <128264476+200ximena@users.noreply.github.com>
2023-08-29 17:40:56 -0500

    Add files via upload

---
+++ datos/cursos.js
@@ -0,0 +1,47 @@
+let infoCursos = {
+    programacion: [
+
+        {
+            id :1,
+            titulo: 'Aprende Java',
+            lenguaje: 'java',
+            vistas: 15000,
+            nivel: 'basico'
+        },
+        {
+            id :2,
+            titulo: 'Aprende Java Avanzado',
+            lenguaje: 'java',
+            vistas: 5000,
+            nivel: 'avanzado'
+        },
+        {
+            id :3,
+            titulo: 'Aprende Node',
+            lenguaje: 'javaScript',
+            vistas: 9000,
+            nivel: 'basico'
+        }
+
+    ],
+    matematicas: [
+
+        {
+            id :1,
+            titulo: 'Aprende Dibujo tecnico',
+            tema: 'dibujo',
+            vistas: 33333,
+            nivel: 'basico'
+        },
+        {
+            id :2,
+            titulo: 'Aprende algebra',
+            tema: 'algebra',
+            vistas: 55655,
+            nivel: 'avanzado'
+        }
+
+    ]
+}
+
+module.exports.infoCursos = infoCursos;
